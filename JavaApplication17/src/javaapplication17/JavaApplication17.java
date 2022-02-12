495a36bed1978bdef7f3f7cf0e6e9225daa24afe
tupa1974 <tupa1974@gmail.com>
2022-02-12 00:12:42 -0300

    Iniciando ejercicio 1

---
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -0,0 +1,22 @@
+package javaapplication17;
+
+
+public class JavaApplication17 {
+
+    /*
+     Una matriz de 5 filas x 3 columnas almacena el total de goles de 5 jugadores de futbol en los últimos 
+    3 partidos que jugaron, donde cada fila representa a un jugador y cada columna a la cantidad de goles 
+    que hizo. Se necesita un programa que sea capaz de permitir la carga de los goles, 
+    calcular el promedio de goles realizado por cada jugador y almacenar el resultado en un vector 
+    de 5 posiciones, donde cada posición representará a un jugador. Tener en cuenta el siguiente 
+    diagrama para llevar a cabo el planteo:
+
+
+Nota: tener en cuenta que el promedio de goles puede dar como resultado un número que 
+    NO SEA ENTERO.
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
